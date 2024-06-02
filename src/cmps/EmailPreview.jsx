51fd2aa309c1ca9c0211f4a158eaa25be7a86fd7
benel2606 <benel2606@gmail.com>
2024-06-02 20:56:13 +0300

    adding new dummy emails

--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -1,31 +1,62 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
-import { AiOutlineStar } from "react-icons/ai";
-import { AiFillStar } from "react-icons/ai";
-import { MdDelete } from "react-icons/md";
+import { Link } from "react-router-dom"
+import { AiOutlineStar } from "react-icons/ai"
+import { AiFillStar } from "react-icons/ai"
+import { MdDelete } from "react-icons/md"
 import { emailService } from "../services/email.service.js"
 
-export function EmailPreview({ email, onRemoveEmail,onIsRead ,onToggleStarred}) {
-    function formattedDate(timestamp) {
-        const date = new Date(timestamp);
-        const formattedDate = date.toLocaleDateString()
-        return formattedDate
-    }
+export function EmailPreview({
+  email,
+  onRemoveEmail,
+  onIsRead,
+  onToggleStarred,
+}) {
+  function formattedDate(timestamp) {
+    const date = new Date(timestamp)
+    const formattedDate = date.toLocaleDateString()
+    return formattedDate
+  }
 
-    return (
-        <article className={email.isRead===true?"email-preview":"email-preview is-read"}>
-            <div className="email-preview-container">
-                <input type="checkbox"></input>
-                <div className="star-icon-btn" onClick={onToggleStarred}>
-                    {email.isStarred===true?<AiFillStar className="react-icon" color={"rgb(214 222 7)"} size={20}/>:<AiOutlineStar className="react-icon" color={"#00000073"} size={20}/>}
-                </div>
-                <Link to={`/email/${email.id}`} onClick={onIsRead}>
-                    <div>{email.subject}</div>
-                    <div>{email.body}</div>
-                    <div>{formattedDate(email.sentAt)}</div>
-                </Link>
-            </div>
-            <div onClick={() => onRemoveEmail(email.id)}><MdDelete className="react-icon before-padding" size={20}/></div>
-        </article>
-    )
+  return (
+    <article
+      className={
+        email.isRead === true ? "email-preview" : "email-preview is-read"
+      }
+    >
+      <div className="email-preview-container">
+        <input type="checkbox"></input>
+        <div className="star-icon-btn" onClick={onToggleStarred}>
+          {email.isStarred === true ? (
+            <AiFillStar
+              className="react-icon"
+              color={"rgb(214 222 7)"}
+              size={20}
+            />
+          ) : (
+            <AiOutlineStar
+              className="react-icon"
+              color={"#00000073"}
+              size={20}
+            />
+          )}
+        </div>
+        <Link to={`/email/${email.id}`} onClick={onIsRead}>
+          <div>{email.fromName}</div>
+          <div>
+            {email.subject}
+            <span>
+              -{" "}
+              {email.body.length > 100
+                ? email.body.substring(0, 100) + "..."
+                : email.body}
+            </span>
+          </div>
+          <div>{formattedDate(email.sentAt)}</div>
+        </Link>
+      </div>
+      <div onClick={() => onRemoveEmail(email.id)}>
+        <MdDelete className="react-icon before-padding" size={20} />
+      </div>
+    </article>
+  )
 }
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -60,25 +60,68 @@ function createMail(subject = '', body = '', isRead = false, isStarred=false, se
 function _createMails() {
     let emails = utilService.loadFromStorage(STORAGE_KEY)
     if (!emails || !emails.length) {
-        const emails = [{ 
-            id: 'e101',
-            subject: 'Miss you!',
-            body: 'Would love to catch up sometimes', 
-            isRead: false, 
-            isStarred: false, 
-            sentAt : 1551133930594, 
-            removedAt : null, //for later use from: 'momo@momo.com', to: 'user@appsus.com' 
-        },
-        {
-            id: 'e102',
-            subject: 'Hello!',
-            body: 'How are you?', 
-            isRead: false, 
-            isStarred: false, 
-            sentAt : 1551133930599, 
-            removedAt : null, //for later use from: 'momo@momo.com', to: 'user@appsus.com' 
-        }
-    ]
+        const emails =[
+            {
+                "fromEmail": "sed.dolor@protonmail.ca",
+                "toEmail": "iaculis.aliquet@icloud.ca",
+                "fromName": "Lois Martinez",
+                "toName": "Jolie Park",
+                "id": "NVI51ZSU9HL",
+                "subject": "nunc ullamcorper eu",
+                "body": "neque. Sed eget lacus. Mauris non dui nec urna suscipit",
+                "isStarred": false,
+                "isRead": false,
+                "sentAt": 1702121830000
+            },
+            {
+                "fromEmail": "nibh.dolor@aol.edu",
+                "toEmail": "sapien.cursus@aol.ca",
+                "fromName": "Vincent Leon",
+                "toName": "Palmer Webb",
+                "id": "VSP54PED7PK",
+                "subject": "aliquam",
+                "body": "est mauris, rhoncus id, mollis nec, cursus a, enim. Suspendisse aliquet, sem ut cursus luctus, ipsum leo elementum sem, vitae aliquam eros turpis non enim. Mauris quis turpis vitae purus gravida sagittis. Duis gravida. Praesent eu nulla at sem molestie sodales. Mauris blandit enim",
+                "isStarred": false,
+                "isRead": true,
+                "sentAt": 1692681843000
+            },
+            {
+                "fromEmail": "non.lobortis@yahoo.org",
+                "toEmail": "in.aliquet@aol.ca",
+                "fromName": "Farrah Knight",
+                "toName": "Fitzgerald Stone",
+                "id": "MVP20PDF2PW",
+                "subject": "Maecenas libero est",
+                "body": "hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus.",
+                "isStarred": true,
+                "isRead": false,
+                "sentAt": 1701695441000
+            },
+            {
+                "fromEmail": "lorem@aol.com",
+                "toEmail": "ultrices@yahoo.com",
+                "fromName": "Larissa Blake",
+                "toName": "Keefe Rivers",
+                "id": "UKJ83DNJ8HN",
+                "subject": "Suspendisse aliquet",
+                "body": "rutrum lorem ac risus. Morbi metus. Vivamus euismod urna. Nullam lobortis quam a felis ullamcorper viverra. Maecenas iaculis",
+                "isStarred": true,
+                "isRead": true,
+                "sentAt": 1738635793000
+            },
+            {
+                "fromEmail": "tortor@outlook.ca",
+                "toEmail": "magna@hotmail.couk",
+                "fromName": "Grady Randolph",
+                "toName": "Sylvia Livingston",
+                "id": "GXV52YYI6MT",
+                "subject": "Aenean sed",
+                "body": "sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum",
+                "isStarred": true,
+                "isRead": false,
+                "sentAt": 1727177520000
+            }
+        ]
         utilService.saveToStorage(STORAGE_KEY, emails)
     }
 }
